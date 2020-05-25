572d51dbcf396100f6f934b9fb39e45dfe7783eb
Malte Bünz <malte.buenz@deutschebahn.com>
2020-05-25 09:43:39 +0200

    remove failing test

--- Wegli/WegliTests/WegliTests.swift
+++ Wegli/WegliTests/WegliTests.swift
@@ -18,10 +18,6 @@ class WegliTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        XCTAssertEqual("Hello World!".count, 1)
-    }
-
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         measure {
